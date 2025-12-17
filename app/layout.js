6d241de89564a59d19f36eb77753cc6861e8efbd
Mohan-Badiger <mohanbadiger250@gmail.com>
2025-12-17 11:09:31 +0530

    Added Hero Section

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 
 const poppins = Poppins({
-  weight:'500'
+  weight:'400'
 })
 
 export const metadata = {
--- components/AuthButton.js
+++ components/AuthButton.js
@@ -25,7 +25,7 @@ export default function AuthButton({ user }) {
       <Button
         onClick={() => setShowAuthModal(true)}
         variant="default"
-        size="lg"
+        size="sm"
         className="bg-orange-500 hover:bg-orange-600 gap-2"
       >
         <LogIn className="w-4 h-4" />
